6bba623cee0acdc2dc45a6182c635d04155aca2d
Mehdi <mehdi.sapin@gmail.com>
2023-11-22 23:32:27 +0100

    wip: popup style

--- logcash.popup.js
+++ logcash.popup.js
@@ -26,7 +26,6 @@ popup.setStyle = function(elems) {
 	elems.popupRemaining.style.top = "80px"; // remove
 	elems.popupRemaining.style.left = "20px"; // remove
 	
-	// elems.popupTopDiv.style.height = "30px";
 	elems.popupTopDiv.style.cursor = "move";
 	elems.popupTopDiv.style.height = "fit-content";
 	elems.popupTopDiv.style.width = "100%";
@@ -36,24 +35,23 @@ popup.setStyle = function(elems) {
 	elems.popupTopDiv.style.flexDirection = "column";
 	elems.popupTopDiv.style.justifyContent = "flex-start";
 	elems.popupTopDiv.style.color = "#9b9b9b";
-	elems.popupTopDiv.style.padding = "6px 30px 6px 6px";
-	// elems.popupTopDiv.style.alignItems = "center";
+	// elems.popupTopDiv.style.padding = "6px 30px 6px 6px";
 	elems.popupTopDiv.style.whiteSpace = "nowrap";
 
 	elems.popupTopLeftText.style.color = "#e2e2e2";
-	elems.popupTopLeftText.style.fontSize = "0.8em";
+	elems.popupTopLeftText.style.fontSize = "0.9em";
 	elems.popupTopLeftText.style.fontWeight = "bold";
-	elems.popupTopLeftText.style.marginBottom = "2px";
 	elems.popupTopLeftText.style.pointerEvents = "none";
-	elems.popupTopLeftText.style.margin = "0";
-	elems.popupTopLeftText.style.padding = "0 4px";
+	// elems.popupTopLeftText.style.margin = "0 0 2px 0";
+	// elems.popupTopLeftText.style.padding = "0 1px";
 	elems.popupTopLeftText.style.textShadow = "rgb(0, 0, 0) 0px 0px 3px";
 	
-	elems.popupTopRightText.style.color = "#9b9b9b";
-	elems.popupTopRightText.style.fontSize = "0.6em";
-	elems.popupTopRightText.style.margin = "0";
-	elems.popupTopRightText.style.padding = "0 4px";
-	elems.popupTopRightText.style.pointerEvents = "none";
+	// elems.popupTopRightText.style.color = "#9b9b9b";
+	elems.popupTopRightText.style.fontSize = "0.7em";
+	// elems.popupTopRightText.style.margin = "0";
+	// elems.popupTopRightText.style.padding = "2px 1px 2px 2px";
+	// elems.popupTopRightText.style.pointerEvents = "none";
+	// elems.popupTopRightText.style.pointerEvents = "pointerenter";
 	elems.popupTopRightText.style.textShadow = "rgb(0, 0, 0) 0px 0px 3px";
 }
 
